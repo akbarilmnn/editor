46002fd86c1963e38831413fb1a07d6a7bf27b5b
akbar <akbarsmp27@gmail.com>
2023-03-23 22:14:44 +0700

    added a plugins which list all the mapping in keymaps.lua in which-key.lua.

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,17 @@
+return {
+    -- source repository
+    "folke/which-key.nvim",
+    -- initialize 
+    -- can either use this or using a config function 
+    --opts = {}
+    config = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+        require("which-key").setup({
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        })
+    end
+}
+
