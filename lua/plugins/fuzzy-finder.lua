1a207b9ae39e14caf09fb1473b157e0f8b82621e
akbar <akbarsmp27@gmail.com>
2023-03-23 22:31:13 +0700

    added a plugin telescope.nvim located in telescope.lua that is used to fuzzy find files, string buffers, lsp, etc.

---
+++ lua/plugins/fuzzy-finder.lua
@@ -0,0 +1,16 @@
+return {
+    -- source repository 
+    "nvim-telescope/telescope.nvim",
+    
+    -- dependencies 
+    -- plenary.nvim 
+    -- burntsushi/ripgrep -> not a neovim plugin 
+    -- sharkdp/fd -> not a neovim plugin
+    -- nvim-tree/nvim-web-devicons
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+    },
+
+    -- command to run 
+    cmd = "Telescope" 
+}
