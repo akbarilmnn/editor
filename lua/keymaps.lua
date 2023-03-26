f22155e5d4a0d70c1cc49364683e26eb4e4e66c8
akbar <akbarsmp27@gmail.com>
2023-03-26 12:03:21 +0700

    make a helper function to create better keymaps with descriptive names.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,54 +1,55 @@
-local map = vim.api.nvim_set_keymap
+---@diagnostic disable: unused-function
+local set_keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 vim.g.mapleader = ' '
 
+-- TODO: make a helper function to make easier keymaps with description
+
+local set_keymap = function(mode,key_bindings,cmd,desc)
+    vim.api.nvim_set_keymap(mode,key_bindings,cmd, {
+        noremap = true,
+        silent = true,
+        desc = desc,
+    })
+end
+
 -- save
-map('n', '<leader>s', ':w<CR>', opts)
+set_keymap('n', '<leader>s', '<cmd>w<CR>', "shortcut to save a file")
 -- save and quit
-map('n', '<leader>q', ':wq<CR>', opts)
+set_keymap('n', '<leader>q', '<cmd>wq<CR>', "shortcut to save and quit a file")
 -- go to visual mode easily
-map('n', '<leader>v', '<C-v>', opts)
+set_keymap('n', '<leader>v', '<C-v>', "short to go to visual mode")
 -- just quit
-map('n', '<leader>e', ':q!<CR>', opts)
+set_keymap('n', '<leader>e', ':q!<CR>', "shortcut to quit without saving anything")
 
 -- use nerd tree because it can hijack netrw
-map('n', '<leader>t', ':NvimTreeToggle<CR>', opts)
+set_keymap('n', '<leader>t', ':NvimTreeToggle<CR>', "toggle the file tree")
 
--- shitty autopair lol
-map('i', 'jj', '<Esc>', opts)
-map('i', 'jk', '<Esc>:', opts)
---map('i', '(', '()<left>', opts)
---map('i', '{', '{}<left>', opts)
---map('i', '[', '[]<left>', opts)
---map('i', "'", "''<left>", opts)
---map('i', '"', '""<left>', opts)
---map('i', '`', '``<left>',opts)
---map('i', '<', '<><left>',opts)
---map('i', '|', '||<left>', opts)
---map('n', 'sh', '<Esc>:!', opts )
+set_keymap('i', 'jj', '<Esc>', "shortcut to normal mode")
+set_keymap('i', 'jk', '<Esc>:',"shortcut to command mode")
 
 -- toggle buffers
-map('n', '<C-n>', ':bNext<CR>', opts)
-map('n', '<C-b>', ':bprevious<CR>', opts)
+set_keymap('n', '<C-n>', '<cmd>bNext<CR>', "go to the next buffer")
+set_keymap('n', '<C-b>', '<cmd>bprevious<CR>', "go to the previous buffer")
 
 -- can move blocks of code by using visua mode
-map("v", "J", ":m '>+1<CR>gv=gv", opts)
-map("v", "K", ":m '<-2<CR>gv=gv", opts)
+set_keymap("v", "J", ":m '>+1<CR>gv=gv", "move a block of code up")
+set_keymap("v", "K", ":m '<-2<CR>gv=gv", "move a block of code down")
 
 
 -- visual mode
-map('v', "<", "<gv", opts)
-map('v', ">", ">gv", opts)
+set_keymap('v', "<", "<gv", "move a block of code into the left")
+set_keymap('v', ">", ">gv", " move a block of code into the right")
 
 
 -- telescope keymaps 
 -- telescope
-map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", opts) -- find files within current working directory, respects .gitignore
-map("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", opts) -- find string in current working directory as you type
-map("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", opts) -- find string under cursor in current working directory
-map("n", "<leader>fb", "<cmd>Telescope buffers<cr>", opts) -- list open buffers in current neovim instance
-map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", opts) -- list available help help_tags
+set_keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", "find files within current working directory respects, .gitignore") 
+set_keymap("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", "find string in current working directory as you type") 
+set_keymap("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", "find string under cursor in current working directory") 
+set_keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", "list open buffers in current neovim instance")
+set_keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", "list available help help_tags")
 
 
 
